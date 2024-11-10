47ff275c82d74fbab39057f0b1042179aba82a72
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-10 20:10:05 +0000

    Add view to products app views.py for book list, add urls.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def index(request):
+
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+
+    return render(request, 'products/books.html', context)

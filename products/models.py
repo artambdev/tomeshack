3a471cf702a5762b7e1648334378027ea6d09bd4
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-10 17:52:45 +0000

    Remove sku field of book model from models.py

--- products/models.py
+++ products/models.py
@@ -13,8 +13,7 @@ class Category(models.Model):
 
 class Book(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
+    title = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stars = models.IntegerField(null=True, blank=True)

55f0e7e1f1d14d17cd0c7b15ece45f48bd35d927
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-10 17:25:57 +0000

    Add category model to products app models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    visible_name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_visible_name(self):
+        return self.visible_name

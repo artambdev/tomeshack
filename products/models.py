bf0052ff5e38675f9848f4116f3063e731fb652b
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-10 17:49:49 +0000

    Add book model to models.py, fix book titles in dataset

--- products/models.py
+++ products/models.py
@@ -10,3 +10,14 @@ class Category(models.Model):
 
     def get_visible_name(self):
         return self.visible_name
+
+class Book(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stars = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name

ec4b1938456e4e8901859bbfb1475cd3cc424e43
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-13 18:56:19 +0000

    Add model to products models.py for reviews

--- products/models.py
+++ products/models.py
@@ -13,6 +13,7 @@ class Category(models.Model):
     def get_visible_name(self):
         return self.visible_name
 
+
 class Book(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     uid = models.SlugField(max_length=200, default=None, null=True)
@@ -26,3 +27,31 @@ class Book(models.Model):
     
     def get_description(self):
         return f'"{self.description[:40]}..."'
+
+
+class Post(models.Model):
+    """
+    Class to define the review model
+    """
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews"
+    )
+    uid = models.SlugField(max_length=200, default=None, null=True)
+    review_of = models.ForeignKey(
+        Book,
+        on_delete=models.SET_NULL,
+        related_name="reviews"
+    )
+    content = models.TextField(max_length=200)
+    stars = models.IntegerField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    hidden = models.BooleanField()
+
+    def Meta(self):
+        """
+        Show the newest reviews first in admin screen
+        """
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f'"{self.content[:15]}..." | written by {self.author}'

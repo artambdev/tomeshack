87da7e392fe49d748fb32d8fee2359683e02b48f
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-18 20:42:23 +0000

    Add view and urls for modifying quantity in cart view

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('cart/', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('modify/<item_id>/', views.modify_cart, name='modify_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -5,6 +5,25 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 
+def modify_cart(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
 def add_to_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -16,6 +35,5 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
     

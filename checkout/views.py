a689198b3edf87ad8b27ac52a8b51f8e5318b96c
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-24 19:57:26 +0000

    Add order submission handling to checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Book
 from cart.contexts import cart_contents
 
 import stripe
@@ -11,26 +13,68 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "You don't have anything in your cart.")
-        return redirect(reverse('products'))
+    if request.method == POST:
+        cart = request.session.get('cart', {})
 
-    current_cart = cart_contents(request)
-    total = current_cart['total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
 
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret_key': intent.client_secret,
-    }
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    book = Book.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            book=book,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                except Book.DoesNotExist:
+                    messages.error(request, (
+                        "One of the books in your cart wasn't found in our database!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
 
-    return render(request, template, context)
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "You don't have anything in your cart.")
+            return redirect(reverse('products'))
+
+        current_cart = cart_contents(request)
+        total = current_cart['total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY
+        )
+
+        order_form = OrderForm()
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret_key': intent.client_secret,
+        }
+
+        return render(request, template, context)

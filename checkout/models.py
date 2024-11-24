dfe3fa40411a1c65aa25424d56ec28066577d2b4
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-24 16:42:52 +0000

    Create model for orders

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
 
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
 
+    country = models.CharField(max_length=30, null=False, blank=False)
+    postcode = models.CharField(max_length=16, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=40, null=False, blank=False)
+    street_address2 = models.CharField(max_length=40, null=True, blank=True)
+    county = models.CharField(max_length=25, null=True, blank=True)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)

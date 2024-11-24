e042ce3c96698f36b50fc9ed60a736fcf996b84f
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-24 20:39:36 +0000

    Register checkout app and include urls in main urls.py

--- tomeshack/urls.py
+++ tomeshack/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
 ]

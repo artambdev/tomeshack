2a0ff8964c7b19706db81e65a4918e50018f6bd3
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-24 19:48:56 +0000

    Add payment handling to stripe elements js

--- tomeshack/settings.py
+++ tomeshack/settings.py
@@ -152,3 +152,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Stripe-related settings
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')

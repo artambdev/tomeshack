2aff838a2b7c891c24a18812ee094ef2e4041d75
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-24 17:07:57 +0000

    Create signals for line item creation/deletion and import in apps

---
+++ checkout/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    When update/creating line item, update the line total
+    """
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    """
+    When deleting a line item, update the line total
+    """
+    instance.order.update_total()

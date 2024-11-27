c9ce1847f1cf6042b55edde3e1316f00f521001b
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-27 19:58:56 +0000

    Add test_views.py for home app

---
+++ home/test_views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.test import TestCase
+
+
+class TestListViews(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="userName",
+            password="passWord",
+            email="sample@email.com"
+        )
+
+
+    def test_render_index(self):
+        """
+        Test that the index works
+        """
+        response = self.client.get(reverse(
+            'home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Tomeshack", response.content)

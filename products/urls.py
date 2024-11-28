6fb528516d5218840bcebd783bd74f3a4ffa649c
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-28 18:22:58 +0000

    Fix repetition in product list url

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.book_list, name='products'),
+    path('', views.book_list, name='products'),
     path('<slug:uid>/', views.view_book, name='view_book'),
     path('<slug:uid>/delete/', views.delete_review, name='delete_review'),
     path('<slug:uid>/hide/', views.hide_review, name='hide_review'),

24868aaedd7a38577f00a62f4ad396b07a1b61ad
Arthur Ambalov <arthurambalov@gmail.com>
2024-11-28 18:08:12 +0000

    Fix broken review count variable name on view_book.html

--- tomeshack/settings.py
+++ tomeshack/settings.py
@@ -116,17 +116,21 @@ WSGI_APPLICATION = 'tomeshack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+
+DATABASES = {
+    'default': dj_database_url.parse('postgresql://neondb_owner:FaVfWT8UId9n@ep-holy-grass-a2tl1ccg.eu-central-1.aws.neon.tech/faced_bash_taps_515224')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

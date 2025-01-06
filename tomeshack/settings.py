75fa9c47d8059046c93f4971b630c2d920b57360
Arthur Ambalov <arthurambalov@gmail.com>
2025-01-06 18:45:53 +0000

    Fix commented out staticfiles dir

--- tomeshack/settings.py
+++ tomeshack/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-artambdev-tomeshack-8hy649ycxsi.ws-eu117.gitpod.io', '.herokuapp.com']
 
@@ -166,7 +166,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
